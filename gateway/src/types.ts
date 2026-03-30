505f44de694889d8cc692dd3cacece785f4e4318
Krystof Matejka <krystof.matejka@pm.me>
2026-03-30 20:46:36 +0200

    version 4

--- gateway/src/types.ts
+++ gateway/src/types.ts
@@ -1,5 +1,18 @@
-interface WebServer {
+/**
+ * METHOD: POST
+ * PATH: /api/state
+ * Payload
+ */
 
+interface WebServerStateRequest {
+  value: 'on' | 'off';
+  deviceId: string;
+}
+
+interface WebServerCommandRequest {
+  command: 'on' | 'off';
+  deviceId: string;
+  gatewayId: string;
 }
 
 interface Gateway {
@@ -9,3 +22,16 @@ interface Gateway {
 interface Esp32 {
 
 }
+
+/*
+Protocol: TCP/IP
+Payload: INT[]
+0 = on
+1 = off
+*/
+
+/**
+ * METHOD: POST
+ * PATH: /api/:state
+ * state: on | off
+ */
